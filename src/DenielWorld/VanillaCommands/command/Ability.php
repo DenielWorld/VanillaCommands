9f601c932265754c15e73478c2c4c327172ef722
jasonwynn10 <jasonwynn10@me.com>
2019-12-30 13:36:21 -0500

    Cleaned up Ability command

--- src/DenielWorld/VanillaCommands/command/Ability.php
+++ src/DenielWorld/VanillaCommands/command/Ability.php
@@ -6,8 +6,9 @@ use pocketmine\command\PluginCommand;
 use DenielWorld\VanillaCommands\Loader;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginIdentifiableCommand;
-use pocketmine\utils\TextFormat as TF;
-use pocketmine\utils\Config;
+use pocketmine\command\utils\InvalidCommandSyntaxException;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 class Ability extends PluginCommand implements PluginIdentifiableCommand {
 
@@ -17,66 +18,32 @@ class Ability extends PluginCommand implements PluginIdentifiableCommand {
     {
         parent::__construct($name, $owner);
         $this->setPermission("vanillacommands.command.ability");
-        $this->setUsage("ability <player> <ability> <true/false>");
+        $this->setUsage("/ability <player: player> <ability: string> <value: bool>");
         $this->setDescription("Sets a player's ability.");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        $cfg = new Config($this->getPlugin()->getDataFolder() . "config.yml", Config::YAML);
-        foreach ($this->reg_abilities as $ability) {
-            if (isset($args[0])) {
-                $player = $this->getPlugin()->getServer()->getPlayer($args[0]);
-                if(!$player){
-                    return false;
-                }
-                elseif (isset($args[1]) and $args[1] == $ability) {
-                    if (isset($args[2]) and is_bool($args[2])) {
-                        if ($args[2] == true) {
-                            $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true); //wonder what true/false is for
-                        } elseif ($args[2] == false) {
-                            if ($player->hasPermission("vanillacommands.state." . $args[1])) {
-                                $attachment = $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
-                                $player->removeAttachment($attachment);
-                            } else {
-                                return false;
-                            }
-                        } else {
-                            if ($cfg->get("ability-default-bool") == true) {
-                                $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
-                            } elseif ($cfg->get("ability-default-bool") == false) {
-                                if ($player->hasPermission("vanillacommands.state." . $args[1])) {
-                                    $attachment = $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
-                                    $player->removeAttachment($attachment);
-                                } else {
-                                    return false;
-                                }
-                            } else {
-                                //True is the backup default value
-                                $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
-                            }
-                        }
-                    } else {
-                        if ($cfg->get("ability-default-bool") == true) {
-                            $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
-                        } elseif ($cfg->get("ability-default-bool") == false) {
-                            if ($player->hasPermission("vanillacommands.state." . $args[1])) {
-                                $attachment = $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
-                                $player->removeAttachment($attachment);
-                            } else {
-                                $sender->sendMessage(TF::RED . "Please define if the ability will be set to true or false for the given player");
-                            }
-                        } else {
-                            $sender->sendMessage(TF::RED . "Default value not found, please check your config or have an Admin check config");
-                        }
-                    }
-                } else {
-                    $sender->sendMessage(TF::RED . "Please assign a valid ability");
-                }
-            } else {
-                $sender->sendMessage(TF::RED . "Please assign a valid player");
+        if(count($args) < 3)
+            throw new InvalidCommandSyntaxException();
+        if(!in_array(strtolower($args[1]), $this->reg_abilities)) {
+            $sender->sendMessage(TextFormat::RED."Ability must be one of: ".implode(", ", $this->reg_abilities));
+            return;
+        }
+        if(!is_bool($args[2]))
+            throw new InvalidCommandSyntaxException();
+        $player = $this->getPlugin()->getServer()->getPlayer($args[0]);
+        if(!$player instanceof Player) {
+            $sender->sendMessage(TextFormat::RED."Player offline!");
+            return;
+        }
+        if ($args[2]) {
+            $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
+        } else {
+            if ($player->hasPermission("vanillacommands.state." . $args[1])) {
+                $attachment = $player->addAttachment($this->getPlugin(), "vanillacommands.state." . $args[1], true);
+                $player->removeAttachment($attachment);
             }
         }
-        return true;
     }
 }
