94e38aac8aeaaa330ba3ba306e11950208eb7435
jasonwynn10 <jasonwynn10@me.com>
2019-12-30 14:41:28 -0500

    mob events clean up

--- src/DenielWorld/VanillaCommands/command/MobEvent.php
+++ src/DenielWorld/VanillaCommands/command/MobEvent.php
@@ -6,39 +6,37 @@ use DenielWorld\VanillaCommands\Loader;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\command\utils\InvalidCommandSyntaxException;
+use pocketmine\utils\TextFormat;
 use pocketmine\utils\TextFormat as TF;
 
 class MobEvent extends PluginCommand implements PluginIdentifiableCommand{
 
-    private $legalmobevents;
-
-    private $plugin;
-
     public function __construct(string $name, Loader $owner)
     {
         parent::__construct($name, $owner);
-        $this->setDescription("Control what mob events are allowed to run");
-        $this->setUsage("/mobevent <event> <bool>");
+        $this->setDescription("Controls what mob events are allowed to run");
+        $this->setUsage("/mobevent <event: mobEvent> [value: Boolean]");
         $this->setPermission("vanillacommands.command.mobevent");
-        $this->plugin = $owner;
-        $this->legalmobevents = $owner->getLegalMobEvents();
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        foreach($this->legalmobevents as $legalmobevent){
-            if(isset($args[0])) continue;
-            if($args[0] == $legalmobevent and !isset($args[1])) {
-                $sender->sendMessage(TF::RED . "Please provide a bool argument");
-            }
-            elseif($args[0] == $legalmobevent and isset($args[1]) and $args[1] == true){
-                $this->plugin->addMobEvent($args[0]);
-                $sender->sendMessage(TF::GREEN . $args[0] . "is now set to true");
-            }
-            elseif($args[0] == $legalmobevent and isset($args[1]) and $args[1] == false){
-                $this->plugin->removeMobEvent($args[0]);
-                $sender->sendMessage(TF::GREEN . $args[0] . "is now set to false");
-            }
-        }
+	    if(!isset($args[0]))
+	    	throw new InvalidCommandSyntaxException();
+    	if(!in_array(strtolower($args[0]), $this->getPlugin()->getLegalMobEvents())) {
+		    $sender->sendMessage(TextFormat::RED."Ability must be one of: ".implode(", ", $this->getPlugin()->getLegalMobEvents()));
+    		return;
+	    }
+    	$value = true;
+    	if(isset($args[1]) and is_bool($args[1]))
+    		$value = $args[1];
+    	if($value) {
+		    $this->getPlugin()->addMobEvent(strtolower($args[0]));
+		    $sender->sendMessage(TF::GREEN . $args[0] . "is now set to true");
+	    }else{
+		    $this->getPlugin()->removeMobEvent(strtolower($args[0]));
+		    $sender->sendMessage(TF::GREEN . $args[0] . "is now set to false");
+	    }
     }
 }
